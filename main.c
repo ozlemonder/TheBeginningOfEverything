3c8a9e0c81ee1d243f8b0d02eab05b1a9d0fb32a
ozlemonder <44654016+ozlemonder@users.noreply.github.com>
2018-10-31 23:55:43 +0300

    İlk Comment
    İlk comment benden.
    
    -Fatih

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*  //FOR DÖNGÜSÜ
+	int i,toplam;
+    toplam=0;
+    for(i=1;i<=10;i++){
+    	toplam+=i;
+	}
+	printf("toplam= %d",toplam);
+	
+	
+	//WHÝLE DÖNGÜSÜ
+	int i=0,sayi,toplam=0;
+	printf("bir sayi girin:");
+	scanf("%d",&sayi);	
+	
+	while(i<sayi){
+		toplam+=i*i;
+		i++;
+	}
+	printf("toplam:%d",toplam);
+	
+	// DO - WHILE
+		int i=0,sayi,toplam=0;
+	printf("bir sayi girin:");
+	scanf("%d",&sayi);
+	
+	do{
+		toplam+=i*i;
+		i++;	
+	}while(i<sayi);
+	
+	printf("Toplam: %d",toplam);
+	
+	//yýldýz
+	
+	int i,j,satir;
+	printf("satir sayisi: ");
+	scanf("%d",&satir);
+	
+	for(i=0;i<satir;i++){
+		j=0;
+		while(j<=i){
+			printf(" * ");
+			j++;
+		}
+		printf("\n");
+	}
+	
+	
+*/
+int main() {
+	
+	
+	
+	
+}
